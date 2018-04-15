acf1b81577a196546f61e00478ad7eb35441ae8d
Jerry Yu <jerryy@andrew.cmu.edu>
2018-04-14 20:53:19 -0400

    Add image load and greyscale

---
+++ code/Sequential/main.py
@@ -0,0 +1,17 @@
+from scipy.misc import imsave
+from scipy.ndimage import imread
+import numpy as np
+
+img = imread("Banana.jpg")
+
+print np.shape(img)
+(width, height, color) = np.shape(img)
+
+greyscale = np.zeros((width, height))
+
+for row in range(width):
+    for col in range(height):
+        greyscale[row][col] = sum(img[row][col]) / 3
+
+imsave("Banana_gray.jpg", greyscale)
+imsave("Banana_segmented.jpg", img)
