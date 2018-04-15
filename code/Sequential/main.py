1a02dd1f9269775a0eddaeb67a94e74399d1c187
Jerry Yu <jerryy@andrew.cmu.edu>
2018-04-14 21:13:49 -0400

    Add blur for line energy

--- code/Sequential/main.py
+++ code/Sequential/main.py
@@ -5,13 +5,37 @@ import numpy as np
 img = imread("Banana.jpg")
 
 print np.shape(img)
-(width, height, color) = np.shape(img)
+(height, width, color) = np.shape(img)
 
-grayscale = np.zeros((width, height))
+# Convert to grayscale
+grayscale = np.zeros((height, width))
 
-for row in range(width):
-    for col in range(height):
+for row in range(height):
+    for col in range(width):
         grayscale[row][col] = sum(img[row][col]) / 3
 
-imsave("Banana_gray.jpg", greyscale)
+
+
+# Blur with box filter, take average of square around center pixel
+box_size = 10
+blurred = np.zeros((height, width))
+for row in range(height):
+    for col in range(width):
+        total = 0
+        count = 0
+        for i in range(row - (box_size / 2), row + (box_size / 2)):
+            for j in range(col - (box_size / 2), col + (box_size / 2)):
+                if (i < 0 or i >= height or j < 0 or j >= width):
+                    continue
+                count += 1
+                total += grayscale[i][j]
+        if count == 0:
+            count = 1
+        blurred[row][col] = (total / count)
+
+# Line Energy
+line_energy = blurred
+
+
+imsave("Banana_blurred.jpg", blurred)
 imsave("Banana_segmented.jpg", img)
