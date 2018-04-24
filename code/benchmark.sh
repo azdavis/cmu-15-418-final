68ca9071dc5320f0757bc2f1752127d08201a5fb
Ariel Davis <ariel.z.davis@icloud.com>
2018-04-23 21:08:26 -0400

    do not use options

--- code/benchmark.sh
+++ code/benchmark.sh
@@ -3,8 +3,8 @@ set -efu
 make main-c main-cu
 
 for image in elephant.ppm; do
-    time ./main-c img/$image -o 1
-    time ./main-cu img/$image -o 2
+    time ./main-c img/$image 1
+    time ./main-cu img/$image 2
     if cmp 1 2; then
         echo "Results match: $image"
     else
