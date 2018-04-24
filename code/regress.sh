0de387af3c7da52ef805f609a0c84a712db179fb
Jerry Yu <jerryy@andrew.cmu.edu>
2018-04-24 13:11:25 -0400

    Add regress for small scale accuracy testing

---
+++ code/regress.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -efu
+
+make main-c main-cu
+
+for image in elephant.ppm; do
+    time ./main-c img/$image 1
+    time ./main-cu img/$image 2
+    if cmp 1 2; then
+        echo "Results match: $image"
+    else
+        echo "Results differ: $image"
+    fi
+done
