9f7eb9ac7a572b3af7c08a685da9c0eef51dc476
Ariel Davis <ariel.z.davis@icloud.com>
2018-04-24 11:59:35 -0400

    clang-format main.cu

--- code/main.cu
+++ code/main.cu
@@ -1,6 +1,6 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 #include <string>
 
@@ -24,157 +24,150 @@ static inline void cudaCheck(cudaError_t code, const char *file, int line) {
     exit(EXIT_FAILURE);
 }
 #else
-#define CUDA_CHECK ((void) 0)
+#define CUDA_CHECK ((void)0)
 #endif
 
-
 typedef struct {
-     int xmin, xmax, ymin, ymax;
+    int xmin, xmax, ymin, ymax;
 } range;
 
 // PPM reading writing code guided from:
 // https://stackoverflow.com/questions/2693631/read-ppm-file-and-store-it-in-an-array-coded-with-c
 
 typedef struct {
-     unsigned char red,green,blue;
+    unsigned char red, green, blue;
 } PPMPixel;
 
 typedef struct {
-     int width, height;
-     PPMPixel *data;
+    int width, height;
+    PPMPixel *data;
 } PPMImage;
 
 static inline __host__ __device__ int div_ceil(int n, int d) {
     return (n + (d - 1)) / d;
 }
 
-static PPMImage *readPPM(const char *filename)
-{
+static PPMImage *readPPM(const char *filename) {
     char buff[16];
     PPMImage *img;
     FILE *fp;
     int c, rgb_comp_color;
-    //open PPM file for reading
+    // open PPM file for reading
     fp = fopen(filename, "rb");
     if (!fp) {
         fprintf(stderr, "Unable to open file '%s'\n", filename);
         exit(1);
     }
 
-    //read image format
+    // read image format
     if (!fgets(buff, sizeof(buff), fp)) {
-      perror(filename);
-      exit(1);
+        perror(filename);
+        exit(1);
     }
 
-    //check the image format
+    // check the image format
     if (buff[0] != 'P' || buff[1] != '6') {
         fprintf(stderr, "Invalid image format (must be 'P6')\n");
         exit(1);
     }
 
-    //alloc memory form image
+    // alloc memory form image
     img = (PPMImage *)malloc(sizeof(PPMImage));
     if (!img) {
-         fprintf(stderr, "Unable to allocate memory\n");
-         exit(1);
+        fprintf(stderr, "Unable to allocate memory\n");
+        exit(1);
     }
 
-    //check for comments
+    // check for comments
     c = getc(fp);
     while (c == '#') {
-    while (getc(fp) != '\n') ;
-         c = getc(fp);
+        while (getc(fp) != '\n')
+            ;
+        c = getc(fp);
     }
 
     ungetc(c, fp);
-    //read image size information
+    // read image size information
     if (fscanf(fp, "%d %d", &img->width, &img->height) != 2) {
-         fprintf(stderr, "Invalid image size (error loading '%s')\n", filename);
-         exit(1);
+        fprintf(stderr, "Invalid image size (error loading '%s')\n", filename);
+        exit(1);
     }
 
-    //read rgb component
+    // read rgb component
     if (fscanf(fp, "%d", &rgb_comp_color) != 1) {
-         fprintf(stderr, "Invalid rgb component (error loading '%s')\n", filename);
-         exit(1);
+        fprintf(stderr, "Invalid rgb component (error loading '%s')\n", filename);
+        exit(1);
     }
 
-    //check rgb component depth
-    if (rgb_comp_color!= RGB_COMPONENT_COLOR) {
-         fprintf(stderr, "'%s' does not have 8-bits components\n", filename);
-         exit(1);
+    // check rgb component depth
+    if (rgb_comp_color != RGB_COMPONENT_COLOR) {
+        fprintf(stderr, "'%s' does not have 8-bits components\n", filename);
+        exit(1);
     }
 
-    while (fgetc(fp) != '\n') ;
-    //memory allocation for pixel data
-    img->data = (PPMPixel*)malloc(img->width * img->height * sizeof(PPMPixel));
+    while (fgetc(fp) != '\n')
+        ;
+    // memory allocation for pixel data
+    img->data = (PPMPixel *)malloc(img->width * img->height * sizeof(PPMPixel));
 
     if (!img) {
-         fprintf(stderr, "Unable to allocate memory\n");
-         exit(1);
+        fprintf(stderr, "Unable to allocate memory\n");
+        exit(1);
     }
 
-    //read pixel data from file
+    // read pixel data from file
     if (fread(img->data, 3 * img->width, img->height, fp) != img->height) {
-         fprintf(stderr, "Error loading image '%s'\n", filename);
-         exit(1);
+        fprintf(stderr, "Error loading image '%s'\n", filename);
+        exit(1);
     }
 
     fclose(fp);
     return img;
 }
 
-void writePPM(const char *filename, PPMImage *img)
-{
+void writePPM(const char *filename, PPMImage *img) {
     FILE *fp;
-    //open file for output
+    // open file for output
     fp = fopen(filename, "wb");
     if (!fp) {
-         fprintf(stderr, "Unable to open file '%s'\n", filename);
-         exit(1);
+        fprintf(stderr, "Unable to open file '%s'\n", filename);
+        exit(1);
     }
 
-    //write the header file
-    //image format
+    // write the header file
+    // image format
     fprintf(fp, "P6\n");
 
-    //image size
-    fprintf(fp, "%d %d\n",img->width,img->height);
+    // image size
+    fprintf(fp, "%d %d\n", img->width, img->height);
 
     // rgb component depth
-    fprintf(fp, "%d\n",RGB_COMPONENT_COLOR);
+    fprintf(fp, "%d\n", RGB_COMPONENT_COLOR);
 
     // pixel data
     fwrite(img->data, 3 * img->width, img->height, fp);
     fclose(fp);
 }
 
-static PPMPixel* getPixel(int x, int y, PPMImage *img)
-{
+static PPMPixel *getPixel(int x, int y, PPMImage *img) {
     return &(img->data[x + y * img->width]);
 }
 
-static void setPixel(int x, int y, PPMImage *img,
-                     unsigned char R, unsigned char G, unsigned char B)
-{
+static void setPixel(int x, int y, PPMImage *img, unsigned char R,
+                                         unsigned char G, unsigned char B) {
     img->data[x + y * img->width].red = R;
     img->data[x + y * img->width].green = G;
     img->data[x + y * img->width].blue = B;
 }
 
-static int getBucketIdx(int r, int g, int b)
-{
+static int getBucketIdx(int r, int g, int b) {
     return r * BUCKETS * BUCKETS + g * BUCKETS + b;
 }
 
-__global__ void blur(int width, int height,
-                     PPMPixel *imgData,
-                     float *blurKernel,
-                     PPMPixel *blurData,
-                     char *mask) {
+__global__ void blur(int width, int height, PPMPixel *imgData,
+                                         float *blurKernel, PPMPixel *blurData, char *mask) {
 
-    int col =  blockIdx.x * blockDim.x + threadIdx.x;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
     int row = blockIdx.y * blockDim.y + threadIdx.y;
 
     if (row < 0 || row >= height || col < 0 || col >= width) {
@@ -186,16 +179,15 @@ __global__ void blur(int width, int height,
     float red = 0;
     float green = 0;
     float blue = 0;
-    for (i_k = 0; i_k < FILTER_SIZE; i_k++){
-        for (j_k = 0; j_k < FILTER_SIZE; j_k++){
-            float weight = blurKernel[i_k*FILTER_SIZE + j_k];
+    for (i_k = 0; i_k < FILTER_SIZE; i_k++) {
+        for (j_k = 0; j_k < FILTER_SIZE; j_k++) {
+            float weight = blurKernel[i_k * FILTER_SIZE + j_k];
             int i = row - (FILTER_SIZE / 2) + i_k;
             int j = col - (FILTER_SIZE / 2) + j_k;
 
             if (i < 0 || i >= height || j < 0 || j >= width) {
                 continue;
-            }
-            else if (mask[i * width + j] == 1) {
+            } else if (mask[i * width + j] == 1) {
                 continue;
             }
             PPMPixel pt = imgData[width * i + j];
@@ -207,9 +199,9 @@ __global__ void blur(int width, int height,
     }
 
     if (count != 0) {
-        blurData[row*width + col].red = (unsigned char) (red / count);
-        blurData[row*width + col].green = (unsigned char) (green / count);
-        blurData[row*width + col].blue = (unsigned char) (blue / count);
+        blurData[row * width + col].red = (unsigned char)(red / count);
+        blurData[row * width + col].green = (unsigned char)(green / count);
+        blurData[row * width + col].blue = (unsigned char)(blue / count);
     }
 }
 
@@ -229,53 +221,49 @@ __host__ int main(int argc, char **argv) {
     printf("Initializing CUDA for CudaRenderer\n");
     printf("Found %d CUDA devices\n", deviceCount);
 
-    for (int i=0; i<deviceCount; i++) {
+    for (int i = 0; i < deviceCount; i++) {
         cudaDeviceProp deviceProps;
         cudaGetDeviceProperties(&deviceProps, i);
         name = deviceProps.name;
 
         printf("Device %d: %s\n", i, deviceProps.name);
         printf("   SMs:        %d\n", deviceProps.multiProcessorCount);
-        printf(
-            "   Global mem: %.0f MB\n",
-            static_cast<float>(deviceProps.totalGlobalMem) / (1024 * 1024)
-        );
+        printf("   Global mem: %.0f MB\n",
+                     static_cast<float>(deviceProps.totalGlobalMem) / (1024 * 1024));
         printf("   CUDA Cap:   %d.%d\n", deviceProps.major, deviceProps.minor);
     }
 
     PPMImage *img = readPPM(infile);
 
-    int *color_counts = (int*) malloc(BUCKETS * BUCKETS * BUCKETS * sizeof(int));
+    int *color_counts = (int *)malloc(BUCKETS * BUCKETS * BUCKETS * sizeof(int));
     if (color_counts == NULL)
         exit(1);
-    char *oldMask = (char*) calloc(img->width * img->height, sizeof(char));
+    char *oldMask = (char *)calloc(img->width * img->height, sizeof(char));
     if (oldMask == NULL)
         exit(1);
-    char *mask = (char*) calloc(img->width * img->height, sizeof(char));
+    char *mask = (char *)calloc(img->width * img->height, sizeof(char));
     if (mask == NULL)
         exit(1);
-    float *blurKernel = (float*) malloc(FILTER_SIZE * FILTER_SIZE * sizeof(float));
+    float *blurKernel =
+            (float *)malloc(FILTER_SIZE * FILTER_SIZE * sizeof(float));
     if (blurKernel == NULL)
         exit(1);
-    PPMPixel *blurData = (PPMPixel*) calloc(img->width * img->height, sizeof(PPMPixel));
+    PPMPixel *blurData =
+            (PPMPixel *)calloc(img->width * img->height, sizeof(PPMPixel));
     if (blurData == NULL)
         exit(1);
 
     PPMPixel *cudaImgData;
     cudaMalloc(&cudaImgData, img->width * img->height * sizeof(PPMPixel));
-    cudaMemcpy(cudaImgData,
-               img->data,
-               img->width * img->height * sizeof(PPMPixel),
-               cudaMemcpyHostToDevice
-    );
+    cudaMemcpy(cudaImgData, img->data,
+                         img->width * img->height * sizeof(PPMPixel),
+                         cudaMemcpyHostToDevice);
 
     PPMPixel *cudaBlurData;
     cudaMalloc(&cudaBlurData, img->width * img->height * sizeof(PPMPixel));
-    cudaMemcpy(cudaBlurData,
-               img->data,
-               img->width * img->height * sizeof(PPMPixel),
-               cudaMemcpyHostToDevice
-    );
+    cudaMemcpy(cudaBlurData, img->data,
+                         img->width * img->height * sizeof(PPMPixel),
+                         cudaMemcpyHostToDevice);
 
     // Even box blur
     for (int i = 0; i < FILTER_SIZE; i++) {
@@ -285,11 +273,8 @@ __host__ int main(int argc, char **argv) {
     }
     float *cudaBlurKernel;
     cudaMalloc(&cudaBlurKernel, FILTER_SIZE * FILTER_SIZE * sizeof(float));
-    cudaMemcpy(cudaBlurKernel,
-               blurKernel,
-               FILTER_SIZE * FILTER_SIZE * sizeof(float),
-               cudaMemcpyHostToDevice
-    );
+    cudaMemcpy(cudaBlurKernel, blurKernel,
+                         FILTER_SIZE * FILTER_SIZE * sizeof(float), cudaMemcpyHostToDevice);
 
     // Get Walls
     int ltWall = img->width / LTRTWALLDENOM;
@@ -298,12 +283,18 @@ __host__ int main(int argc, char **argv) {
 
     // Get color distribution
     range rs[3];
-    rs[0].xmin = 0; rs[0].xmax = ltWall;
-    rs[0].ymin = 0; rs[0].ymax = img->height;
-    rs[1].xmin = rtWall; rs[1].xmax = img->width;
-    rs[1].ymin = 0; rs[1].ymax = img->height;
-    rs[2].xmin = 0; rs[2].xmax = img->width;
-    rs[2].ymin = 0; rs[2].ymax = tpWall;
+    rs[0].xmin = 0;
+    rs[0].xmax = ltWall;
+    rs[0].ymin = 0;
+    rs[0].ymax = img->height;
+    rs[1].xmin = rtWall;
+    rs[1].xmax = img->width;
+    rs[1].ymin = 0;
+    rs[1].ymax = img->height;
+    rs[2].xmin = 0;
+    rs[2].xmax = img->width;
+    rs[2].ymin = 0;
+    rs[2].ymax = tpWall;
 
     int i, j, ri;
     for (ri = 0; ri < 3; ri++) {
@@ -312,16 +303,14 @@ __host__ int main(int argc, char **argv) {
             for (j = r.xmin; j < r.xmax; j++) {
                 PPMPixel *pt = getPixel(j, i, img);
                 color_counts[getBucketIdx(pt->red / BUCKET_SIZE,
-                                          pt->green / BUCKET_SIZE,
-                                          pt->blue / BUCKET_SIZE)] += 1;
-
+                                                                    pt->green / BUCKET_SIZE,
+                                                                    pt->blue / BUCKET_SIZE)] += 1;
             }
         }
     }
 
-    int totalBCPix = (ltWall * img->height +
-                     (img->width - rtWall) * img->height +
-                      tpWall * img->width);
+    int totalBCPix = (ltWall * img->height + (img->width - rtWall) * img->height +
+                                        tpWall * img->width);
     int bcThresh = BCTHRESH_DECIMAL * totalBCPix;
 
     for (i = 0; i < img->height; i++) {
@@ -339,20 +328,20 @@ __host__ int main(int argc, char **argv) {
     memcpy(mask, oldMask, img->width * img->height * sizeof(char));
 
     // Clean up mask
-    for (i = 2; i < img->height-2; i++) {
-        for (j = 2; j < img->width-2; j++) {
+    for (i = 2; i < img->height - 2; i++) {
+        for (j = 2; j < img->width - 2; j++) {
             char thisPx = oldMask[i * img->width + j];
             if (thisPx == 0) {
-                int borderSum = (oldMask[(i-1) * img->width + j] +
-                                 oldMask[i * img->width + j-1] +
-                                 oldMask[(i+1) * img->width + j] +
-                                 oldMask[i * img->width + j+1] +
-                                 oldMask[(i-2) * img->width + j] +
-                                 oldMask[i * img->width + j-2] +
-                                 oldMask[(i+2) * img->width + j] +
-                                 oldMask[i * img->width + j+2]);
+                int borderSum = (oldMask[(i - 1) * img->width + j] +
+                                                 oldMask[i * img->width + j - 1] +
+                                                 oldMask[(i + 1) * img->width + j] +
+                                                 oldMask[i * img->width + j + 1] +
+                                                 oldMask[(i - 2) * img->width + j] +
+                                                 oldMask[i * img->width + j - 2] +
+                                                 oldMask[(i + 2) * img->width + j] +
+                                                 oldMask[i * img->width + j + 2]);
                 if (borderSum >= 2) {
-                   mask[i * img->width + j] = 1;
+                    mask[i * img->width + j] = 1;
                 }
             }
         }
@@ -362,41 +351,29 @@ __host__ int main(int argc, char **argv) {
     printf("finished mask, starting blur\n");
     char *cudaMask;
     cudaMalloc(&cudaMask, img->width * img->height * sizeof(char));
-    cudaMemcpy(cudaMask,
-               mask,
-               img->width * img->height * sizeof(char),
-               cudaMemcpyHostToDevice
-    );
+    cudaMemcpy(cudaMask, mask, img->width * img->height * sizeof(char),
+                         cudaMemcpyHostToDevice);
 
     dim3 threadsPerBlock(SQ_DIM, SQ_DIM);
-    dim3 blocks(
-        div_ceil(img->width, SQ_DIM),
-        div_ceil(img->height, SQ_DIM)
-    );
+    dim3 blocks(div_ceil(img->width, SQ_DIM), div_ceil(img->height, SQ_DIM));
 
     CUDA_CHECK;
-    blur<<<blocks, threadsPerBlock>>>(img->width,
-                                      img->height,
-                                      cudaImgData,
-                                      cudaBlurKernel,
-                                      cudaBlurData,
-                                      cudaMask);
+    blur<<<blocks, threadsPerBlock>>>(img->width, img->height, cudaImgData,
+                                                                        cudaBlurKernel, cudaBlurData, cudaMask);
     cudaDeviceSynchronize();
-    cudaMemcpy(blurData,
-               cudaBlurData,
-               img->width * img->height * sizeof(PPMPixel),
-               cudaMemcpyDeviceToHost
-    );
+    cudaMemcpy(blurData, cudaBlurData,
+                         img->width * img->height * sizeof(PPMPixel),
+                         cudaMemcpyDeviceToHost);
     // Put filter on mask
     int height = img->height;
     int width = img->width;
     for (i = 0; i < height; i++) {
         for (j = 0; j < width; j++) {
-            if (mask[i*width + j] == 1) {
+            if (mask[i * width + j] == 1) {
                 PPMPixel *pt = getPixel(j, i, img);
-                blurData[i*width + j].red = pt->red;
-                blurData[i*width + j].green = pt->green;
-                blurData[i*width + j].blue = pt->blue;
+                blurData[i * width + j].red = pt->red;
+                blurData[i * width + j].green = pt->green;
+                blurData[i * width + j].blue = pt->blue;
             }
         }
     }
@@ -415,4 +392,3 @@ __host__ int main(int argc, char **argv) {
     cudaFree(cudaBlurKernel);
     return 0;
 }
-
