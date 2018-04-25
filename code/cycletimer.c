0e3acfc1ed00041445b2aa40f83cde31199a204c
Ariel Davis <ariel.z.davis@icloud.com>
2018-04-24 22:05:15 -0400

    add cycletimer

---
+++ code/cycletimer.c
@@ -0,0 +1,32 @@
+#if defined(__APPLE__)
+#include <mach/mach.h>
+#include <mach/mach_time.h>
+#endif
+#elif _WIN32
+#include <time.h>
+#include <windows.h>
+#else
+#include <sys/time.h>
+#endif
+
+#include <stdint.h>
+
+#include "cycletimer.h"
+
+uint64_t currentTicks() {
+#if defined(__APPLE__)
+    return mach_absolute_time();
+#elif defined(_WIN32)
+    LARGE_INTEGER qwTime;
+    QueryPerformanceCounter(&qwTime);
+    return qwTime.QuadPart;
+#elif defined(__x86_64__)
+    unsigned int a, d;
+    asm volatile("rdtsc" : "=a"(a), "=d"(d));
+    return ((uint64_t)d << 32) + a;
+#else
+    timespec spec;
+    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &spec);
+    return spec.tv_sec * 1000 * 1000 * 1000 + spec.tv_nsec;
+#endif
+}
---
+++ code/cycletimer.h
@@ -0,0 +1,8 @@
+#ifndef CYCLETIMER_H
+#define CYCLETIMER_H
+
+#include <stdint.h>
+
+uint64_t currentSeconds(void);
+
+#endif
