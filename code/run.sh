b6591091bcc4ee13d02bea5c3c0bd4994cb5e316
Ariel Davis <ariel.z.davis@icloud.com>
2018-04-30 23:30:06 -0400

    rework run.sh

--- code/run.sh
+++ code/run.sh
@@ -7,13 +7,18 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-time ./main-c $1 c.ppm
-time ./main-cu $1 cu.ppm
-time ./main-omp $1 omp.ppm
-time ./main-full-cu $1 full-cu.ppm
-if cmp c.ppm cu.ppm && cmp c.ppm omp.ppm && cmp c.ppm full-cu.ppm; then
-    echo "Results match: $1"
-    rm c.ppm cu.ppm omp.ppm
-else
-    echo "Results differ: $1"
-fi
+for x in c cu full-cu omp; do
+    time ./main-$x $1 $x.ppm
+    if [ $x = c ]; then
+        continue
+    fi
+    if cmp c.ppm $x.ppm; then
+        echo "Results for '$x' on '$1' match"
+        rm $x.ppm
+    else
+        echo "Results for '$x' on '$1' differ"
+        exit 1
+    fi
+done
+
+rm c.ppm
