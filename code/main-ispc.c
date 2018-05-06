80bd765518b21f668dcbbc41c0617ba1df6fca45
Ariel Davis <ariel.z.davis@icloud.com>
2018-05-06 01:06:26 -0400

    copy over main.c

--- code/main-ispc.c
+++ code/main-ispc.c
@@ -6,20 +6,15 @@
 #include "lib/etc.h"
 #include "lib/ppm.h"
 
-#include "lib/portrait_ispc.h"
-
 int main(int argc, char **argv) {
     if (argc != 3) {
-        printf("usage: %s <infile> <outfile>\n", argv[0]);
+        fprintf(stderr, "usage: %s <infile> <outfile>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
     char *infile = argv[1];
     char *outfile = argv[2];
 
-    double start;
-
-    printf("begin\n");
-    start = currentSeconds();
+    double start = currentSeconds();
 
     PPMImage *img = readPPM(infile);
     if (img == NULL) {
@@ -28,17 +23,25 @@ int main(int argc, char **argv) {
     int W = img->width;
     int H = img->height;
 
-    printf("load image: %lf\n", currentSeconds() - start);
-    start = currentSeconds();
-
     int ltWall = W / LTRTWALLDENOM;
     int rtWall = (W * (LTRTWALLDENOM - 1)) / LTRTWALLDENOM;
     int tpWall = H / TPWALLDENOM;
 
     int *color_counts = calloc(BUCKETS * BUCKETS * BUCKETS, sizeof(int));
-    if (color_counts == NULL) {
+    char *oldMask = calloc(W * H, sizeof(char));
+    char *mask = calloc(W * H, sizeof(char));
+    float *blurKernel = calloc(FILTER_SIZE * FILTER_SIZE, sizeof(float));
+    PPMPixel *blurData = calloc(W * H, sizeof(PPMPixel));
+    if (color_counts == NULL ||
+        oldMask == NULL ||
+        mask == NULL ||
+        blurKernel == NULL ||
+        blurData == NULL) {
         exit(EXIT_FAILURE);
     }
+
+    printf("{\"init\":%lf", currentSeconds() - start);
+    start = currentSeconds();
 
     range rs[] = {
         {0, ltWall, 0, H},
@@ -52,17 +55,16 @@ int main(int argc, char **argv) {
         for (i = r.ymin; i < r.ymax; i++) {
             for (j = r.xmin; j < r.xmax; j++) {
                 PPMPixel *pt = getPixel(j, i, img);
-                color_counts[
-                    getBucketIdx(
+                int idx = getBucketIdx(
                         pt->red / BUCKET_SIZE,
                         pt->green / BUCKET_SIZE,
-                        pt->blue / BUCKET_SIZE)
-                ] += 1;
+                        pt->blue / BUCKET_SIZE);
+                color_counts[idx] += 1;
             }
         }
     }
 
-    printf("get color_counts: %lf\n", currentSeconds() - start);
+    printf(",\"color_counts\":%lf", currentSeconds() - start);
     start = currentSeconds();
 
     int totalBCPix =
@@ -72,11 +74,7 @@ int main(int argc, char **argv) {
 
     int bcThresh = BCTHRESH_DECIMAL * totalBCPix;
 
-    char *oldMask = calloc(W * H, sizeof(char));
-    if (oldMask == NULL) {
-        exit(EXIT_FAILURE);
-    }
-
+    #pragma omp parallel for shared(i) private(j)
     for (i = 0; i < H; i++) {
         for (j = 0; j < W; j++) {
             PPMPixel *pt = getPixel(j, i, img);
@@ -89,15 +87,12 @@ int main(int argc, char **argv) {
         }
     }
 
-    printf("get oldMask: %lf\n", currentSeconds() - start);
+    printf(",\"build_mask\":%lf", currentSeconds() - start);
     start = currentSeconds();
 
-    char *mask = calloc(W * H, sizeof(char));
-    if (mask == NULL) {
-        exit(EXIT_FAILURE);
-    }
     memcpy(mask, oldMask, W * H * sizeof(char));
 
+    #pragma omp parallel for shared(i) private(j)
     for (i = 2; i < H - 2; i++) {
         for (j = 2; j < W - 2; j++) {
             char this = oldMask[i * W + j];
@@ -118,14 +113,10 @@ int main(int argc, char **argv) {
         }
     }
 
-    printf("get mask: %lf\n", currentSeconds() - start);
+    printf(",\"refine_mask\":%lf", currentSeconds() - start);
     start = currentSeconds();
 
-    printf("finished mask, starting blur\n");
-    float *blurKernel = calloc(FILTER_SIZE * FILTER_SIZE, sizeof(float));
-    if (blurKernel == NULL) {
-        exit(EXIT_FAILURE);
-    }
+    #pragma omp parallel for shared(i) private(j)
     for (i = 0; i < FILTER_SIZE; i++) {
         for (j = 0; j < FILTER_SIZE; j++) {
             int x = (FILTER_SIZE/2) - j;
@@ -136,12 +127,8 @@ int main(int argc, char **argv) {
         }
     }
 
-    PPMPixel *blurData = calloc(W * H, sizeof(PPMPixel));
-    if (blurData == NULL) {
-        exit(EXIT_FAILURE);
-    }
-
     int row, col;
+    #pragma omp parallel for schedule(dynamic) shared(row) private(col)
     for (row = 0; row < H; row++) {
         for (col = 0; col < W; col++) {
             // Foreground Pixel
@@ -181,9 +168,7 @@ int main(int argc, char **argv) {
         }
     }
 
-    printf("get blurData: %lf\n", currentSeconds() - start);
-    start = currentSeconds();
-
+    #pragma omp parallel for shared(i) private(j)
     for (i = 0; i < H; i++) {
         for (j = 0; j < W; j++) {
             if (mask[i * W + j] == 1) {
@@ -198,7 +183,7 @@ int main(int argc, char **argv) {
     PPMPixel *oldData = img->data;
     img->data = blurData;
 
-    printf("use blurData: %lf\n", currentSeconds() - start);
+    printf(",\"blur\":%lf", currentSeconds() - start);
     start = currentSeconds();
 
     errno = 0;
@@ -207,12 +192,11 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
-    printf("write image: %lf\n", currentSeconds() - start);
-
     free(oldData);
     free(color_counts);
     free(blurKernel);
     free(img);
     free(img->data);
+    printf(",\"clean_up\":%lf}", currentSeconds() - start);
     return 0;
 }
