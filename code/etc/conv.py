0a21eac74bb8408b8108b9f458dbaaf0c0685b2c
Ariel Davis <ariel.z.davis@icloud.com>
2018-05-06 20:51:23 -0400

    use format, print_function, stderr

--- code/etc/conv.py
+++ code/etc/conv.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 from scipy.misc import imsave
 from scipy.ndimage import imread
 import numpy as np
 import sys
 
 if len(sys.argv) != 3:
-    print("usage: " + sys.argv[0] + " <infile> <outfile>")
+    print("usage: {} <infile> <outfile>".format(sys.argv[0]), file=sys.stderr)
     sys.exit(1)
 
 infile = sys.argv[1]
